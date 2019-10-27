26399a2f99bd28ac9cafb70481641f310c2d336d
Norrowind <igoranoshin8@gmail.com>
2019-10-27 11:19:51 +0200

    0.0.1 - Added character,animations and running

--- ShotNFall/Source/ShotNFall/ShotNFallGameMode.cpp
+++ ShotNFall/Source/ShotNFall/ShotNFallGameMode.cpp
@@ -7,7 +7,7 @@
 AShotNFallGameMode::AShotNFallGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Character/Logic/BP_SNFCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
